734045a0ce08c0da2b43c9d75bac19d307e6705a
Annanda Sousa <annanda.sousa@gmail.com>
2016-12-06 23:32:17 -0200

    eliminando mais colunas

--- pre_processsing.py
+++ pre_processsing.py
@@ -7,25 +7,25 @@ def eliminando_colunas_mesmo_valor():
     keys_data = data.keys()
     d = {}
     for key_data in keys_data:
-        quantidade_valor = len(data[key_data].value_counts())
-        if quantidade_valor != 1:
+        contagem = data[key_data].value_counts()
+        quantidade_valores = len(contagem)
+        if quantidade_valores > 1 and (contagem.iloc[0] < 70015 and contagem.iloc[1] < 70015):
             d[key_data] = data[key_data].values
-        # if quantidade_valor == 2:
-            # print("Key: {}, valores: {}".format(key_data, data[key_data].value_counts()))
 
     # diminuiu de 370 colunas para 336 colunas
     new_data = pd.DataFrame(d)
-    new_data.to_csv('dataset/train_colunas_limpas.csv')
+    new_data.to_csv('dataset/train_colunas_limpas_2.csv')
 
 
 def eliminando_outliers():
-    data = pd.read_csv('dataset/train_colunas_limpas.csv')
-    keys_data = data.keys()
-    d = {}
-    for key_data in keys_data:
-        quantidade_valor = len(data[key_data].value_counts())
-        if quantidade_valor == 2:
-            print("Key: {}, valores: {}".format(key_data, data[key_data].value_counts()))
+    data = pd.read_csv('dataset/train_colunas_limpas_2.csv')
+    print(data)
+    # keys_data = data.keys()
+    # d = {}
+    # for key_data in keys_data:
+    #     quantidade_valor = len(data[key_data].value_counts())
+    #     if quantidade_valor == 2:
+    #         print("Key: {}, valores: {}".format(key_data, data[key_data].value_counts()))
 
 
 if __name__ == '__main__':
---
+++ relatorio.md
@@ -0,0 +1,7 @@
+# Relatorio
+
+1. Eliminar as colunas cujo conteúdo das células é o mesmo
+    diminuiu de 370 colunas para 336 colunas
+
+2. Eliminar as colunas cujo conteúdo das células é o mesmo para até 70015 linhas
+    diminuiu de 336 colunas para 276 colunas
