111502f0e2a35b038161608d9d7f959e938f355a
Annanda Sousa <annanda.sousa@gmail.com>
2016-12-09 22:45:02 -0200

    fazendo gerar o arquivo de respostas

--- learning.py
+++ learning.py
@@ -1,11 +1,12 @@
 import pandas as pd
-from sklearn.cross_validation import cross_val_score, cross_val_predict
+# from sklearn.cross_validation import cross_val_score, cross_val_predict
+from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, GradientBoostingClassifier
 
 
 def try_classifier(clf, tag):
-    dataset = pd.read_csv('dataset/train_colunas_limpas_2.csv')
-    test = pd.read_csv('dataset/test_colunas_limpas.csv')
+    dataset = pd.read_csv('dataset/train_kmeans.csv')
+    test = pd.read_csv('dataset/test_kmeans.csv')
 
     x = dataset.values[:,:-1]
     y = dataset['TARGET']
@@ -14,15 +15,14 @@ def try_classifier(clf, tag):
 
     clf.fit(x, y)
     predictions = clf.predict_proba(x_test)
-    print(predictions)
-
-    with open('resultados.txt', 'w') as file:
-        for line in predictions:
-            file.write(str(line[1]) + '\n')
 
     scores = cross_val_score(clf, x, y, scoring='roc_auc')
     print("Auc (%s): %0.3f (+/- %0.3f)" % (tag, scores.mean(), scores.std() * 2))
 
+    with open('resultados_3.txt', 'w') as file:
+        for line in predictions:
+            file.write(str(line[1]) + '\n')
+
 
 if __name__ == '__main__':
-    try_classifier(GradientBoostingClassifier(), 'gradientboosting')
+    try_classifier(RandomForestClassifier(), 'gradientboosting')
