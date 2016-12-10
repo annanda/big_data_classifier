c3b1caab360d14eb963b1319604328927eb223d4
Annanda Sousa <annanda.sousa@gmail.com>
2016-12-10 18:19:21 -0200

    arrumando o normalize para nao perder as informações de nome das colunas. e implementando o pearson

--- normalize.py
+++ normalize.py
@@ -4,24 +4,39 @@ import scipy
 
 from sklearn import preprocessing
 
-df = pd.read_csv('./test_file.csv')
+def colunas_apagar_pearson(df, train=1):
+    to_delete = []
+    keys_df = df.keys()
 
-to_delete = []
+    x = df.values
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(x)
+    df = pd.DataFrame(x_scaled)
+    df.columns = keys_df[:]
 
-x = df.values
-min_max_scaler = preprocessing.MinMaxScaler()
-x_scaled = min_max_scaler.fit_transform(x)
-df = pd.DataFrame(x_scaled)
+    if train:
+        range_colunas = len(df.columns)-1
+    else:
+        range_colunas = len(df.columns)
+    for i in range(range_colunas):
+        for j in range(i+1, range_colunas):
+            corr, value = scipy.stats.pearsonr(df.ix[:,i:i+1], df.ix[:,j:j+1])
+            if(corr > 0.9 or corr*-1 > 0.9):
+                to_delete.extend(df.ix[:,i:i+1].keys())
+                break
 
-for i in range (0,len(df.columns)-1 ):
-	for j in range(i+1, len(df.columns)-1):
-		corr, value = scipy.stats.pearsonr(df[i], df[j])
-		if(corr > 0.9 or corr*-1 > 0.9):
-			to_delete.append(i)
-			break
-		
-	
-
-print to_delete
+    return to_delete
 
 
+def apaga_colunas_pearson(to_delete, df, df_name_export):
+    for key in to_delete:
+        df = df.drop(key, 1)
+
+    df.to_csv('dataset/' + df_name_export)
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('dataset/train_file.csv')
+    df_name_export = 'colunas_apagadas_pearson.csv'
+    to_delete = colunas_apagar_pearson(df)
+    apaga_colunas_pearson(to_delete, df, df_name_export)
